0b9ab9c5b0c12d13339d647af2e8f154ddaf08a4
qowuie <pasha12369@yandex.ru>
2021-09-15 01:31:11 +0300

    исправление задачи

--- input_array.py
+++ input_array.py
@@ -1,11 +1,30 @@
 arr = []
-i = input("Введите положительное число или !, чтобы завершить работу\n")
-while i != "!":
-    if i.isdigit() and int(i) != 0:
+while True:
+    i = input()
+    if i.isdigit() and int(i) > 0:
         arr.append(int(i))
-        print(f"Вы ввели число {int(i)}")
+    elif i == '!':
+        if len(arr) < 3:
+            print("Надо ввести как минимум 3 числа")
+            continue
+        else:
+            break
     else:
-        print("Вводить можно только положительные числа, не равные нулю!")
-    i = input("Введите положительное число или !, чтобы завершить работу\n")
+        print("Водите только положительные числа ")
+
+
+triangle = False
+arr.sort(reverse=True)
+
+for i in range(len(arr) - 2):
+    a, b, c = arr[i], arr[i + 1], arr[i + 2]
+    if a + b > c and a + c > b and b + c > a:
+        triangle = True
+        break
+if triangle:
+    p = (a + b + c) / 2
+    s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+    print(a, b, c, s)
 else:
-    print(*arr)
+    print("В заданных числах нет подходящих сторон треугольника")
+
